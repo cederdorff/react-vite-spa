58d54f9f20b99829664cd1ff886b57c5950bdf3a
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-08-28 13:36:22 +0200

    Add initial documentation for React SPA setup

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router";
 
 export default function Nav() {
-    return (
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-        </nav>
-    );
+  return (
+    <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+    </nav>
+  );
 }
